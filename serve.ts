8e6cad0f5a8b61cb80f9eed920be8a2a8bd8d583
rubenkristian <rubenkristian96@gmail.com>
2022-06-03 14:28:27 +0700

    use oak framework

--- serve.ts
+++ serve.ts
@@ -1,2 +1,14 @@
-import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
-serve(_ => new Response("Hello World\n"));
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+const router = new Router();
+router.get('/', async (ctx) => {
+  await ctx.send({
+    root: `${Deno.cwd()}/public`,
+    index: "index.html"
+  });
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({port: 8080});
